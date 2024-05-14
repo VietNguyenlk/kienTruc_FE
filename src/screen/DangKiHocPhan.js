74c353e869eca5194b252500992d8a2ca315a089
viet <viet1282002.com>
2024-05-14 20:27:05 +0700

    dkhp duoc

--- src/screen/DangKiHocPhan.js
+++ src/screen/DangKiHocPhan.js
@@ -1,42 +1,42 @@
 import React, { useEffect, useState } from "react";
 import "../App.css";
-// import { postApiNoneToken } from './api/Api';
-import { getApiLHP, getApiNoneToken } from "../api/Api";
+import { getApiLHP, getApiNoneToken, postApiLHP } from "../api/Api";
 
 function DangKiHocPhan() {
   const [selectedCourseIndex, setSelectedCourseIndex] = useState(null);
   const [selectedLHPIndex, setSelectedLHPIndex] = useState(null);
   const [listSubject, setListSubject] = useState([]);
   const [listLHP, setListLHP] = useState([]);
+  const [listLHPBySv, setListLHPBySv] = useState([]);
+  const maSV = "20000001";
 
-  // STT
   const subjectCounter = 1;
 
-  // show list danh sach mon hoc co the dang ky
   useEffect(() => {
-    const listSubject = async () => {
+    const fetchListSubject = async () => {
       try {
         console.log("listSubject");
         const res = await getApiNoneToken(
           "http://localhost:3001/api/monhoc/getListSubject"
         );
-        console.log(res.data.data);
+        console.log("mon hoc", res.data.data);
         setListSubject(res.data.data);
       } catch (error) {
         console.log("loi", error);
       }
     };
-    listSubject();
+    fetchListSubject();
   }, []);
 
   const handleRadioChange = (index) => {
     setSelectedCourseIndex(index);
   };
+
   const handleRadioChangeLHP = (index) => {
     console.log("index", index);
     setSelectedLHPIndex(index);
   };
-  // get lhp theo mã  mon hoc
+
   const getLHP = async (maHP) => {
     try {
       const response = await getApiLHP("/getListLopHocPhanByMaMonHoc/" + maHP);
@@ -46,6 +46,45 @@ function DangKiHocPhan() {
       console.log("loi lhp", error);
     }
   };
+
+  const registerCourse = async (maLHP) => {
+    try {
+      const response = await postApiLHP("/addSinhVien", {
+        maLopHocPhan: maLHP,
+        maSV: maSV,
+      });
+      console.log(response);
+
+      // After successful registration, fetch the updated list of registered courses
+      getLHPBySv(); 
+    } catch (error) {
+      console.log("loi dang ky", error);
+    }
+  };
+
+  const handleRegisterClick = () => {
+    if (selectedLHPIndex !== null) {
+      const selectedCourse = listLHP[selectedLHPIndex];
+      registerCourse(selectedCourse.maLopHocPhan);
+    } else {
+      alert("Please select a course to register.");
+    }
+  };
+
+  const getLHPBySv = async () => {
+    try {
+      const response = await getApiLHP("/getListLopHocPhanByMaSV/" + maSV);
+      console.log("lhp theo sv", response.data.data);
+      setListLHPBySv(response.data.data);
+    } catch (error) {
+      console.log("Loi get list", error);
+    }
+  };
+
+  useEffect(() => {
+    getLHPBySv();
+  }, []);
+
   return (
     <div className="App">
       <h2>Đăng ký học phần</h2>
@@ -58,14 +97,13 @@ function DangKiHocPhan() {
             <th>Tên Môn Học</th>
             <th>TC</th>
             <th>Bắt buộc</th>
-            <th>học phần tiên quyết</th>
+            <th>Học phần tiên quyết</th>
           </tr>
         </thead>
         <tbody>
           {listSubject.map((course, index) => (
             <tr key={index}>
               <td>
-                {" "}
                 <input
                   type="radio"
                   checked={selectedCourseIndex === index}
@@ -73,9 +111,8 @@ function DangKiHocPhan() {
                   onClick={() => {
                     getLHP(course.maHP);
                   }}
-                ></input>{" "}
+                ></input>
               </td>
-
               <td>{subjectCounter + index}</td>
               <td>{course.maHP}</td>
               <td>{course.tenMonHoc}</td>
@@ -105,20 +142,18 @@ function DangKiHocPhan() {
             {listLHP.map((course, index) => (
               <tr key={index}>
                 <td>
-                  {" "}
                   <input
                     type="radio"
                     checked={selectedLHPIndex === index}
                     onChange={() => handleRadioChangeLHP(index)}
-                    // onClick={() => {getDetailLHP(course)}}
-                  ></input>{" "}
+                  ></input>
                 </td>
                 <td>{subjectCounter + index}</td>
                 <td>{course.maLopHocPhan}</td>
-                <td>{course.maLopHocPhan}</td>
+                <td></td>
                 <td>{course.tenLopHocPhan}</td>
                 <td>{course.soLuongSV}</td>
-                <td>{course.soLuongDaDangKy}</td>
+                <td>{course.danhSachSinhVien.length}</td>
                 <td>{course.tinhTrang}</td>
               </tr>
             ))}
@@ -159,6 +194,7 @@ function DangKiHocPhan() {
           </tbody>
         </table>
         <input
+          onClick={handleRegisterClick}
           type="button"
           value="Đăng ký môn học"
           style={{
@@ -172,7 +208,43 @@ function DangKiHocPhan() {
           }}
         ></input>
       </div>
+      <h2>Các lớp học phần đã đăng ký</h2>
+      <div>
+        <table className="course-table">
+          <thead>
+            <tr>
+              <th></th>
+              <th>STT</th>
+              <th>Mã LHP</th>
+              <th>Tên môn học</th>
+              <th>Lớp dự kiến</th>
+              <th>Số TC</th>
+              <th>Nhóm TH</th>
+              <th>Học Phí</th>
+              <th>Thu</th>
+              <th>Trạng thái LHP</th>
+            </tr>
+          </thead>
+          <tbody>
+            {listLHPBySv.map((course, index) => (
+              <tr key={index}>
+                <td></td>
+                <td>{subjectCounter + index}</td>
+                <td>{course.maLopHocPhan}</td>
+                <td>{course.tenMonHoc}</td>
+                <td>{course.tenLopHocPhan}</td>
+                <td>{course.soTC}</td>
+                <td></td>
+                <td></td>
+                <td>X</td>
+                <td>{course.tinhTrang}</td>
+              </tr>
+            ))}
+          </tbody>
+        </table>
+      </div>
     </div>
   );
 }
+
 export default DangKiHocPhan;
